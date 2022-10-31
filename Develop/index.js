31563b2ea1a548cf2b8d1b7ff82f24b0b1a19c1d
Brevenn <Brevenjh@gmail.com>
2022-10-31 14:59:07 -0500

    began writing out html template with reference to answers

--- Develop/index.js
+++ Develop/index.js
@@ -56,6 +56,40 @@ const questions = [
 inquirer.prompt(questions)
 .then(answers =>{
     console.log(answers)
+    let readMe = `<img src='https://img.shields.io/badge/${answers.license}-blue.svg'>
+
+    <h1> Title </h1>
+    
+    <h2> Description </h2>
+    
+    <h2> Table of Contents </h2>
+    
+    <details open="open">
+    <summary> Table of Contents </summary>
+    <ol>
+    <li><a href="#installation">Installation</a></li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contribution">Contribution</a></li>
+    <li><a href="#tests">Tests</a></li>
+    <li><a href="#questions">Questions</a></li>
+    </details>
+    
+    <h3 id="installation"> Installation </h3>
+    <p> ${answers.installation} </p>
+    
+    <h3 id="usage"> Usage </h3>
+    <p> ${answers.usage} </p>
+    
+    <h3 id="contribution"> Contribution </h3>
+    <p> ${answers.contribution} </p>
+    
+    <h3 id="tests"> Tests </h3>
+    <p> ${answers.tests} </p>
+    
+    <h3 id="questions"> Questions </h3>
+    <p> Feel free to reach out to me at ${answers.email}. To see more of my work, checkout my Github: ${answers.github} </p>`
+
+    fs.writeFile('newReadMe.md', readMe, err => console.log(err))
 })
 .catch(err => console.log(err))
 // TODO: Create a function to write README file
